1b96f1208abe4e0ed82de639cb135bd86e2751bd
appyofficial <appy70890@gmail.com>
2020-02-28 17:24:17 +0100

    v 0.1

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,8 +4,9 @@ import './ColorBox.css';
 export default class ColorBox extends Component {
     render() {
         return (
-            <div>
-
+            <div className='ColorBox' style={{ background: this.props.background }}>
+                <p>{this.props.name}</p>
+                <span>more..</span>
             </div>
         );
     }
--- src/Palette.js
+++ src/Palette.js
@@ -4,11 +4,14 @@ import ColorBox from './ColorBox';
 
 class Palette extends React.Component {
     render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+        ));
         return (
             <div className='Palette'>
                 {/*navbar */}
                 <div className='Palette-colors'>
-                    {/*Box of colors */}
+                    {colorBoxes}
                     <ColorBox />
                 </div>
                 {/*footer */}
